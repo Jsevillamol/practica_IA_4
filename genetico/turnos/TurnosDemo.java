2d8a591207b5c863c8da6d0ffced7cbe5de6181a
Jsevillamol <jsevillamol@outlook.com>
2017-11-28 22:41:42 +0100

    Poco a poco

--- genetico/turnos/TurnosDemo.java
+++ genetico/turnos/TurnosDemo.java
@@ -1,4 +1,4 @@
-package aima.gui.demo.search;
+package genetico.turnos;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -32,144 +32,33 @@ import aima.core.search.uninformed.DepthLimitedSearch;
 import aima.core.search.uninformed.IterativeDeepeningSearch;
 
 /**
- * @author Ravi Mohan
- * 
+ * @author Jaime Sevilla
+ * @author David Rubio
  */
 
-public class NQueensDemo {
-
-	private static final int boardSize = 8;
+public class TurnosDemo {
 
 	public static void main(String[] args) {
 
-		newNQueensDemo();
+		GeneticAlgorithmSearchDemo();
 	}
 
-	private static void newNQueensDemo() {
-
-		nQueensWithDepthFirstSearch();
-		nQueensWithBreadthFirstSearch();
-		nQueensWithRecursiveDLS();
-		nQueensWithIterativeDeepeningSearch();
-		nQueensSimulatedAnnealingSearch();
-		nQueensHillClimbingSearch();
-		nQueensGeneticAlgorithmSearch();
-	}
-
-	private static void nQueensWithRecursiveDLS() {
-		System.out.println("\nNQueensDemo recursive DLS -->");
+	public static void GeneticAlgorithmSearchDemo() {
+		System.out.println("\nTurnos Demo GeneticAlgorithm  -->");
 		try {
-			Problem problem = new Problem(new NQueensBoard(boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			SearchForActions search = new DepthLimitedSearch(boardSize);
-			SearchAgent agent = new SearchAgent(problem, search);
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private static void nQueensWithBreadthFirstSearch() {
-		try {
-			System.out.println("\nNQueensDemo BFS -->");
-			Problem problem = new Problem(new NQueensBoard(boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			SearchForActions search = new BreadthFirstSearch(new TreeSearch());
-			SearchAgent agent = new SearchAgent(problem, search);
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensWithDepthFirstSearch() {
-		System.out.println("\nNQueensDemo DFS -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			SearchForActions search = new DepthFirstSearch(new GraphSearch());
-			SearchAgent agent = new SearchAgent(problem, search);
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensWithIterativeDeepeningSearch() {
-		System.out.println("\nNQueensDemo Iterative DS  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(boardSize), NQueensFunctionFactory.getIActionsFunction(),
-					NQueensFunctionFactory.getResultFunction(), new NQueensGoalTest());
-			SearchForActions search = new IterativeDeepeningSearch();
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensSimulatedAnnealingSearch() {
-		System.out.println("\nNQueensDemo Simulated Annealing  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(boardSize, Config.QUEENS_IN_FIRST_ROW),
-					NQueensFunctionFactory.getCActionsFunction(), NQueensFunctionFactory.getResultFunction(),
-					new NQueensGoalTest());
-			SimulatedAnnealingSearch search = new SimulatedAnnealingSearch(new AttackingPairsHeuristic(),
-					new Scheduler(20, 0.045, 100));
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			System.out.println("Search Outcome=" + search.getOutcome());
-			System.out.println("Final State=\n" + search.getLastSearchState());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void nQueensHillClimbingSearch() {
-		System.out.println("\nNQueensDemo HillClimbing  -->");
-		try {
-			Problem problem = new Problem(new NQueensBoard(boardSize, Config.QUEENS_IN_FIRST_ROW),
-					NQueensFunctionFactory.getCActionsFunction(), NQueensFunctionFactory.getResultFunction(),
-					new NQueensGoalTest());
-			HillClimbingSearch search = new HillClimbingSearch(new AttackingPairsHeuristic());
-			SearchAgent agent = new SearchAgent(problem, search);
-
-			System.out.println();
-			printActions(agent.getActions());
-			System.out.println("Search Outcome=" + search.getOutcome());
-			System.out.println("Final State=\n" + search.getLastSearchState());
-			printInstrumentation(agent.getInstrumentation());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void nQueensGeneticAlgorithmSearch() {
-		System.out.println("\nNQueensDemo GeneticAlgorithm  -->");
-		try {
-			FitnessFunction<Integer> fitnessFunction = NQueensGenAlgoUtil.getFitnessFunction();
-			GoalTest goalTest = NQueensGenAlgoUtil.getGoalTest();
+			FitnessFunction<Profesor> fitnessFunction = TurnosGenAlgoUtil.getFitnessFunction();
+			GoalTest goalTest = TurnosGenAlgoUtil.getGoalTest();
 			// Generate an initial population
-			Set<Individual<Integer>> population = new HashSet<Individual<Integer>>();
+			Set<Individual<Profesor>> population = new HashSet<Individual<Profesor>>();
 			for (int i = 0; i < 50; i++) {
-				population.add(NQueensGenAlgoUtil.generateRandomIndividual(boardSize));
+				population.add(TurnosGenAlgoUtil.generateRandomIndividual(boardSize));
 			}
 
-			GeneticAlgorithm<Integer> ga = new GeneticAlgorithm<Integer>(boardSize,
-					NQueensGenAlgoUtil.getFiniteAlphabetForBoardOfSize(boardSize), 0.15);
+			GeneticAlgorithm<Profesor> ga = new GeneticAlgorithm<Profesor>(boardSize,
+					TurnosGenAlgoUtil.getFiniteAlphabetForBoardOfSize(boardSize), 0.15);
 
 			// Run for a set amount of time
-			Individual<Integer> bestIndividual = ga.geneticAlgorithm(population, fitnessFunction, goalTest, 1000L);
+			Individual<Profesor> bestIndividual = ga.geneticAlgorithm(population, fitnessFunction, goalTest, 1000L);
 
 			System.out.println("Max Time (1 second) Best Individual=\n"
 					+ NQueensGenAlgoUtil.getBoardForIndividual(bestIndividual));
@@ -178,7 +67,7 @@ public class NQueensDemo {
 			System.out.println("Fitness         = " + fitnessFunction.apply(bestIndividual));
 			System.out.println("Is Goal         = " + goalTest.isGoalState(bestIndividual));
 			System.out.println("Population Size = " + ga.getPopulationSize());
-			System.out.println("Itertions       = " + ga.getIterations());
+			System.out.println("Iterations       = " + ga.getIterations());
 			System.out.println("Took            = " + ga.getTimeInMilliseconds() + "ms.");
 
 			// Run till goal is achieved
@@ -192,7 +81,7 @@ public class NQueensDemo {
 			System.out.println("Fitness         = " + fitnessFunction.apply(bestIndividual));
 			System.out.println("Is Goal         = " + goalTest.isGoalState(bestIndividual));
 			System.out.println("Population Size = " + ga.getPopulationSize());
-			System.out.println("Itertions       = " + ga.getIterations());
+			System.out.println("Iterations       = " + ga.getIterations());
 			System.out.println("Took            = " + ga.getTimeInMilliseconds() + "ms.");
 
 		} catch (Exception e) {
--- genetico/turnos/TurnosGenAlgoUtil.java
+++ genetico/turnos/TurnosGenAlgoUtil.java
@@ -12,97 +12,103 @@ import aima.core.util.datastructure.XYLocation;
 
 /**
  * A class whose purpose is to provide static utility methods for solving the
- * n-queens problem with genetic algorithms. This includes fitness function,
+ * exam shift distribution problem with genetic algorithms. This includes fitness function,
  * goal test, random creation of individuals and convenience methods for
  * translating between between an NQueensBoard representation and the Integer list
  * representation used by the GeneticAlgorithm.
  * 
- * @author Ciaran O'Reilly
- * @author Ruediger Lunde
+ * @author Jaime Sevilla
+ * @author David Rubio
  * 
  */
-public class NQueensGenAlgoUtil {
+public class TurnosGenAlgoUtil {
 
-	public static FitnessFunction<Integer> getFitnessFunction() {
-		return new NQueensFitnessFunction();
+	/**************
+    * Problem data
+	*******************/
+	private static final int nExamenes = 8;
+	private static final int nTurnos = 16;
+
+	// Lista de profesores, mas un simbolo especial para representar un turno vacío
+	public static enum Profesor = {ANA, BONIATO, CARLA, DOMINGO, ELISA, FEDERICO, GERTRUDIS, VACIO}
+
+	private static final List<Profesor> PROFESORADO = Collections.unmodifiableList(Arrays.asList(values()));
+  	private static final int NPROFESORES = PROFESORADO.size()-1;
+  	private static final Random RANDOM = new Random();
+
+  	private static final restricciones;
+  	private static final preferencias;
+
+	/****************************
+	* Genetic Algorithm Functions
+	*****************************/
+
+	public static FitnessFunction<Profesor> getFitnessFunction() {
+		return new TurnosFitnessFunction();
 	}
 	
 	public static GoalTest getGoalTest() {
-		return new NQueensGenAlgoGoalTest();
+		return new TurnosGenAlgoGoalTest();
 	}
 	
 
-	public static Individual<Integer> generateRandomIndividual(int boardSize) {
-		List<Integer> individualRepresentation = new ArrayList<Integer>();
-		for (int i = 0; i < boardSize; i++) {
-			individualRepresentation.add(new Random().nextInt(boardSize));
+	public static Individual<Profesor> generateRandomIndividual(int boardSize) {
+		List<Profesor> individualRepresentation = new ArrayList<Profesor>();
+
+		// Inicializamos la representacion con todo turnos vacios
+		for (int i = 0; i < nTurnos; i++) {
+			individualRepresentation.add(Profesor.VACIO);
 		}
-		Individual<Integer> individual = new Individual<Integer>(individualRepresentation);
+		
+		// Seleccionamos aleatoriamente nExamenes turnos y los asignamos a profesores aleatorios
+		for (int i : getRandomSelection(nExamenes, nTurnos)){
+			int randomIndex = RANDOM.nextInt(NPROFESORES);
+			individualRepresentation.set(i, PROFESORADO.get(randomIndex));
+		}
+
+		Individual<Profesor> individual = new Individual<Profesor>(individualRepresentation);
 		return individual;
 	}
 
-	public static Collection<Integer> getFiniteAlphabetForBoardOfSize(int size) {
-		Collection<Integer> fab = new ArrayList<Integer>();
-
-		for (int i = 0; i < size; i++) {
-			fab.add(i);
-		}
-
-		return fab;
+	public static Collection<Profesor> getFiniteAlphabet()) {
+		return Profesor.values();
 	}
 	
-	public static class NQueensFitnessFunction implements FitnessFunction<Integer> {
+	public static class TurnosFitnessFunction implements FitnessFunction<Profesor> {
 
-		public double apply(Individual<Integer> individual) {
+		public double apply(Individual<Profesor> individual) {
 			double fitness = 0;
-
-			NQueensBoard board = getBoardForIndividual(individual);
-			int boardSize = board.getSize();
-
-			// Calculate the number of non-attacking pairs of queens (refer to
-			// AIMA
-			// page 117).
-			List<XYLocation> qPositions = board.getQueenPositions();
-			for (int fromX = 0; fromX < (boardSize - 1); fromX++) {
-				for (int toX = fromX + 1; toX < boardSize; toX++) {
-					int fromY = qPositions.get(fromX).getYCoOrdinate();
-					boolean nonAttackingPair = true;
-					// Check right beside
-					int toY = fromY;
-					if (board.queenExistsAt(new XYLocation(toX, toY))) {
-						nonAttackingPair = false;
-					}
-					// Check right and above
-					toY = fromY - (toX - fromX);
-					if (toY >= 0) {
-						if (board.queenExistsAt(new XYLocation(toX, toY))) {
-							nonAttackingPair = false;
-						}
-					}
-					// Check right and below
-					toY = fromY + (toX - fromX);
-					if (toY < boardSize) {
-						if (board.queenExistsAt(new XYLocation(toX, toY))) {
-							nonAttackingPair = false;
-						}
-					}
-
-					if (nonAttackingPair) {
-						fitness += 1.0;
-					}
-				}
-			}
-
+			fitness = preferenciasFitness(individual) - restriccionesVioladas(individual) + equilibrioFitness(individual);
 			return fitness;
 		}
+
+		/*
+		* Calcula cuantas preferencias satisface el individuo
+		*/
+		private static int preferenciasFitness(Individual<Profesor> individual){
+
+		}
+
+		/*
+		* Calcula cuantas restricciones viola el individuo
+		*/
+		public static int restriccionesVioladas(Individual<Profesor> individual){
+
+		}
+
+		/*
+		* Calcula como de equilibrada es la distribucion de turnos sugerida por el individuo
+		*/
+		private static double equilibrioFitness(Individual<Profesor> individual){
+
+		}
+
 	}
 
-	public static class NQueensGenAlgoGoalTest implements GoalTest {
-		private final NQueensGoalTest goalTest = new NQueensGoalTest();
+	public static class TurnosGenAlgoGoalTest implements GoalTest {
 
-		@SuppressWarnings("unchecked")
 		public boolean isGoalState(Object state) {
-			return goalTest.isGoalState(getBoardForIndividual((Individual<Integer>) state));
+			return TurnosFitnessFunction.restriccionesVioladas((Individual<Profesor>) state)) == 0;
 		}
 	}
 
@@ -116,4 +122,25 @@ public class NQueensGenAlgoUtil {
 
 		return board;
 	}
+
+
+	/*
+	* Devuelve una selección aleatoria uniforme de k números menores o iguales que n
+	* Tomado de https://stackoverflow.com/a/29868630/4841832
+	*/
+	private static int[] getRandomSelection (int k, int n) {
+    if (k > n) throw new IllegalArgumentException(
+        "Cannot choose " + k + " elements out of " + n + "."
+    );
+
+    HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>(2*k);
+    int[] output = new int[k];
+
+    for (int i = 0; i < k; i++) {
+        int j = i + RANDOM.nextInt(n - i);
+        output[i] = (hash.containsKey(j) ? hash.remove(j) : j);
+        if (j > i) hash.put(j, (hash.containsKey(i) ? hash.remove(i) : i));
+    }
+    return output;
+}
 }
